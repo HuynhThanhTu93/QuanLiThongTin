b89e90cd9083dfabb8fa30df1d974d4a411b4264
HuynhThanhTu93 <tu0909493091@gmail.com>
2024-08-12 20:27:56 +0700

    Them code

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace QuanLiThongTin
 {
@@ -10,7 +11,22 @@ namespace QuanLiThongTin
             InitializeComponent();
         }
         SqlConnection con = new SqlConnection(@"Data Source=19-LAB402\SQLEXPRESS;Initial Catalog=QuanLiThongTin;Integrated Security=True;Encrypt=False");
-        
+        private void openCon()
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            } 
+                
+        }
+        private void closeCon()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
